fcc25d3364fefd9dab588b59d40e3b64ff8c90fd
Arsh <patel.arsh.k.128@gmail.com>
2022-07-19 20:48:47 +0530

    add start script

--- app.js
+++ app.js
@@ -117,8 +117,10 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log("it's 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`it is ${port}`);
 })
 
 
